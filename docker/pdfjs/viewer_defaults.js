411a14bb60c138f925500d59eebb78915bf8eca3
MBGaruda <44886363+MBGaruda@users.noreply.github.com>
2026-08-16 15:35:56 +0900

    pdfjsの初期値を設定

--- docker/pdfjs/viewer_defaults.js
+++ docker/pdfjs/viewer_defaults.js
@@ -1,9 +1,36 @@
-console.info(
-  "SW2.5 PDF.js viewer_defaults.js LOADED"
-);
+console.info("SW2.5 PDF.js diagnostics loaded");
 
 document.addEventListener("webviewerloaded", () => {
-  console.info(
-    "SW2.5 PDF.js webviewerloaded RECEIVED"
-  );
+  const app = window.PDFViewerApplication;
+
+  app.initializedPromise.then(() => {
+    const log = label => {
+      const viewer = app.pdfViewer;
+
+      console.log(label, {
+        scale: viewer?.currentScaleValue,
+        scrollMode: viewer?.scrollMode,
+        spreadMode: viewer?.spreadMode,
+      });
+    };
+
+    const events = [
+      "pagesinit",
+      "documentinit",
+      "scalechanging",
+      "scrollmodechanged",
+      "spreadmodechanged",
+      "updateviewarea",
+      "documentloaded",
+    ];
+
+    for (const name of events) {
+      app.eventBus.on(name, event => {
+        console.log(`PDF.js event: ${name}`, event);
+        log(`state after ${name}`);
+      });
+    }
+
+    log("state after initializedPromise");
+  });
 });
